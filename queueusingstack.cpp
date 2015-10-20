84b01fc3db21f1ce5b31e840d0390227a02ea182
prernabellara <prerna.bell@gmail.com>
2015-10-19 23:03:36 -0400

    Update queueusingstack.cpp

--- queueusingstack.cpp
+++ queueusingstack.cpp
@@ -19,9 +19,16 @@ void pop()
                         deq.push(i);
                         enq.pop();
                 }
+                while(!deq.empty())
+                {
+                        int i=deq.top();
+                        enq.push(i);
+                        deq.pop();
+                }
 
         }
-        deq.pop();
+
+//      deq.pop();
 }
 int main()
 {
@@ -31,11 +38,13 @@ int main()
         push(3);
 
         pop();
+        push(2);
+        pop();
         std::cout<<"Printing the stack"<<std::endl;
-        while(!deq.empty())
+        while(!enq.empty())
         {
-                std::cout<<deq.top()<<std::endl;
-                deq.pop();
+                std::cout<<enq.top()<<std::endl;
+                enq.pop();
         }
         return 1;
 }
